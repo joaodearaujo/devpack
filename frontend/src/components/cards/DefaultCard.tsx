c8733cdd7bcc0c26b2470ad9b932cd10139c58d9
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-09-16 20:19:00 -0300

    feat(frontend): add default card component

---
+++ frontend/src/components/cards/DefaultCard.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+import { cn } from "../../lib/utils";
+
+export default function DefaultCard({children, className} : {children: ReactNode; className?: string}) {
+    return  (
+        <div className={cn("border border-app-border-strong rounded-md p-3 bg-card-bg", className)}>
+            {children}
+        </div>
+    )
+}
